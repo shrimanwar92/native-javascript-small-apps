1f49081853c5d8b315528b39c6db4220ded0e263
shrimanwar92 <shrimanwar92@gmail.com>
2021-01-24 00:02:17 +0530

    added messages screen

--- chat-app-ui/app.js
+++ chat-app-ui/app.js
@@ -1,6 +1,10 @@
-window.onload = function() {
+function loadUsers() {
 	const list = document.querySelector(".chat-app__users ul");
 	const usersHtml = users.map(user => `<li>${user.name}</li>`).join("");
 	list.innerHTML = usersHtml;
+}
+
+window.onload = function() {
+	loadUsers();
 }
 
