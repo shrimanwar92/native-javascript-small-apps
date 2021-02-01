2b1d7d19a023c662db7444554250a094bd9252c0
shrimanwar92 <shrimanwar92@gmail.com>
2021-02-01 11:17:52 +0530

    added some functionality

--- chat-app-ui/app.js
+++ chat-app-ui/app.js
@@ -1,10 +1,57 @@
-function loadUsers() {
+window.onload = function() {
+	const chat = new Chat();
+	chat.loadUsers();
+}
+
+function Chat() {
+	this.messagesDom = document.querySelector(".chat-app__messages");
+	const inputBox = document.querySelector(".chat-app__input");
+	const sendBtn = document.querySelector(".chat-app__submit");
+	const users = document.querySelector(".chat-app__users");
+	
+	this.messages = new Proxy([], {
+		get: function(target, property, receiver) {
+			return Reflect.get(target, property, receiver);
+		},
+		set: function(target, property, value, receiver) {
+			inputBox.value = "";
+			return Reflect.set(target, property, value);
+		}
+	});
+
+	inputBox.addEventListener("keyup", (event) => {
+		if(event.keyCode == 13) {
+			this.sendMessage(inputBox.value);
+		}
+	});
+
+	sendBtn.addEventListener("click", (event) => {
+		this.sendMessage(inputBox.value);
+	});
+
+	users.addEventListener("click", (event) => {
+		const liArr = users.querySelectorAll("li");
+		Array.from(liArr).forEach(node => {
+			node.classList.remove("active");
+		});
+		event.target.classList.add("active");
+		this.setCurrentUser(event.target.textContent);
+	});
+}
+
+Chat.prototype.loadUsers = function() {
 	const list = document.querySelector(".chat-app__users ul");
 	const usersHtml = users.map(user => `<li>${user.name}</li>`).join("");
 	list.innerHTML = usersHtml;
 }
 
-window.onload = function() {
-	loadUsers();
+Chat.prototype.sendMessage = function(message) {
+	this.messages.push(message);
+	const span = document.createElement("span");
+	span.innerHTML = message;
+	this.messagesDom.appendChild(span);
 }
 
+Chat.prototype.setCurrentUser = function(user) {
+	this.currentUser = user;
+}
